216e14157f4a758731ee449acc1393e57865a6fb
Zain Javed <zainjhere@gmail.com>
2022-03-31 02:22:34 +0500

    added fb login

--- passport.js
+++ passport.js
@@ -1,5 +1,6 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const GitHubStrategy = require('passport-github2').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const passport = require("passport");
 require('dotenv').config();
 
@@ -18,6 +19,16 @@ passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
     callbackURL: "/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    done(null, profile);
+  }
+));
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_CLIENT_ID,
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    callbackURL: "/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     done(null, profile);
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -40,4 +40,11 @@ router.get("/github/callback", passport.authenticate("github", {
 	failureRedirect: "/login/failed"
 }))
 
+router.get("/facebook", passport.authenticate("facebook", {scope: ["profile"]}));
+
+router.get("/facebook/callback", passport.authenticate("facebook", {
+	successRedirect: CLIENT_URL,
+	failureRedirect: "/login/failed"
+}))
+
 module.exports = router;
